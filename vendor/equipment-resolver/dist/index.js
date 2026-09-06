f9c589afb3a3b939f14e28b569ace3da94a6084e
jmwalker3-boop <jmwalker3@me.com>
2026-09-06 07:43:23 -0400

    Export machineConversion helpers from index

--- vendor/equipment-resolver/dist/index.js
+++ vendor/equipment-resolver/dist/index.js
@@ -32,3 +32,4 @@ export { normalizeEquipmentTag } from './equipmentAliases.js';
 export { checkEquipmentGap } from './equipmentGap.js';
 export { needsLoadRounding, roundToOwnedLoad } from './loadRounding.js';
 export { resolveMovementForAthlete, resolveWorkoutForAthlete } from './resolve.js';
+export { convertDistance, machineScaleOptions, CAP_MACHINE_LABEL } from './machineConversion.js';
