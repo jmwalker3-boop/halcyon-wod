78ce36cf9475b9459697f3d35f910a0ec82d0fc9
jmwalker3-boop <jmwalker3@me.com>
2026-09-06 07:44:36 -0400

    Export machineConversion type declarations from index

--- vendor/equipment-resolver/dist/index.d.ts
+++ vendor/equipment-resolver/dist/index.d.ts
@@ -3,3 +3,4 @@ export { normalizeEquipmentTag } from './equipmentAliases.js';
 export { checkEquipmentGap } from './equipmentGap.js';
 export { needsLoadRounding, roundToOwnedLoad } from './loadRounding.js';
 export { resolveMovementForAthlete, resolveWorkoutForAthlete } from './resolve.js';
+export { convertDistance, machineScaleOptions, CAP_MACHINE_LABEL, type CapMachine } from './machineConversion.js';
